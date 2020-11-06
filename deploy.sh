237d2d46102bb67d3e68d32510779ef5d7ca5e7d
王成林 <2998271566@qq.com>
2020-11-06 14:40:32 +0800

    代码更新

--- deploy.sh
+++ deploy.sh
@@ -6,35 +6,21 @@ set -e
 # 生成静态文件
 npm run build
 
+git add -A
+git commit -m "代码更新"
+git push -f https://codeup.aliyun.com/kros/ll_lin/deploy-test.git master
+
+
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# deploy to github
-echo 'b.xugaoyi.com' > CNAME
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
-else
-  msg='来自github actions的自动部署'
-  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
-  git config --global user.name "xugaoyi"
-  git config --global user.email "894072666@qq.com"
-fi
+# deploy to codeup
 git init
 git add -A
-git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
+git commit -m "部署更新"
+git push -f https://codeup.aliyun.com/kros/ll_lin/deploy-test.git master
+curl --header "Content-Type: application/json" --request POST --data '{}' http://flow-openapi.aliyun.com/pipeline/webhook/7sUxOuMAPSoor7acp4gE  # 触发部署流水线
 
-# deploy to coding
-echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:xgy/xgy.git
-else
-  codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
-fi
-git add -A
-git commit -m "${msg}"
-git push -f $codingUrl master # 推送到coding
 
 cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
