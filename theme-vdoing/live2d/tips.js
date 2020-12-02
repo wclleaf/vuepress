069431d375392a9d4f59346b0a7a16b72cee0946
王成林 <2998271566@qq.com>
2020-12-02 17:50:38 +0800

    1

--- theme-vdoing/live2d/tips.js
+++ theme-vdoing/live2d/tips.js
@@ -9,7 +9,7 @@ export default {
   }],
   click: [{
 	  selector: '.vue-live2d-main',
-	  texts: ['再黯淡的日子都可以被你的笑容点亮', '我喜欢和你在一起时的我', '你使我完整', '我想用我的全部，换取一条接近你的路', '有些人就是四，除了二，还是二']
+	  texts: ['再黯淡的日子都可以被你的笑容点亮', '我喜欢和你在一起时的我', '你使我完整', '我想用我的全部，换取一条接近你的路']
   }],
   mouseover: [{
     selector: '.vue-live2d-tool .icon-enjoy',
