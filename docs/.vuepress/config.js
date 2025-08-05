322020f436f9a080651f3b80c17ad4f97c622f29
ext.wangchenglin7 <ext.wangchenglin7@jd.com>
2025-08-05 15:00:20 +0800

    feat: 1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -67,7 +67,7 @@ module.exports = {
     blogger: { // 博主信息，显示在首页侧边栏
       avatar: 'http://oss.chenglin.online/blog/wechat',
       name: 'Kros',
-      slogan: '心之所向，素履以往，生如逆旅，一苇以航'
+      slogan: '凡心所向，素履以往，生如逆旅，一苇以航'
     },
     social: { // 社交图标，显示于博主信息栏和页脚栏
       iconfontCssFile: '//at.alicdn.com/t/font_2180906_jugic8c4cx.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
